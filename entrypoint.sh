a4df320088c8039df272e1515ae521d91f4bc12c
Syaiful Bahri <syai.bahri6@gmail.com>
2025-02-05 16:48:04 +0700

    fix duplicate ssh option

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ if [[ ! -z ${SSH_PRIVATE_KEY+x} ]]; then
   echo "$SSH_PRIVATE_KEY" >> id_rsa
   chmod 600 ./id_rsa
 
-  ssh -4 -4 -f -N -g -o StrictHostKeyChecking=no -o IdentitiesOnly=yes -i ./id_rsa -L 4646:$NOMAD_IP_ADDR:4646 root@$NOMAD_IP_SERVER
+  ssh -4 -f -N -g -o StrictHostKeyChecking=no -o IdentitiesOnly=yes -i ./id_rsa -L 4646:$NOMAD_IP_ADDR:4646 root@$NOMAD_IP_SERVER
 fi
 
 if [[ -z "${LEVANT_VAR_FILE}" ]]; then
